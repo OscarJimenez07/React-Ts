a44c73f0898de0c8830313beb7275240ef9f8004
oscarjimenez07 <ojavierjimenez@uts.edu.co>
2025-01-03 14:51:52 -0500

    Actualizacion de ruta

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Inicio from './pages/Inicio';
-import Acerca from './pages/Acerca';
-import Contacto from './pages/Contacto';
-import Contador from './pages/Contador'
-import Formulario from './components/FormularioRegistro'
-import ComponentePadre from './components/componentePadre';
-import ComPadreRMemo from './components/ComPadreRMemo'
-import LayLoading from './components/LazyLoadingP'
-import LazyLoaingImagen from './components/LazyLoadingImagen'
-import ImagenesSinLazy from './components/ImagenesSinLazy'
-import AnidarComponentes from './components/AnidarComponentes'
-import RenderListas from './components/RenderListas'
+// import Acerca from './pages/Acerca';
+// import Contacto from './pages/Contacto';
+// import Contador from './pages/Contador'
+// import Formulario from './components/FormularioRegistro'
+// import ComponentePadre from './components/componentePadre';
+// import ComPadreRMemo from './components/ComPadreRMemo'
+// import LayLoading from './components/LazyLoadingP'
+// import LazyLoaingImagen from './components/LazyLoadingImagen'
+// import ImagenesSinLazy from './components/ImagenesSinLazy'
+// import AnidarComponentes from './components/AnidarComponentes'
+// import RenderListas from './components/RenderListas'
 import FormShipping from './components/FormShipping'
 
 const App: React.FC = () => {
@@ -19,8 +19,8 @@ const App: React.FC = () => {
     <Router basename="/React-Ts">
       <nav>
         <ul>
-          <li><Link to="/">Inicio</Link></li>
-          <li><Link to="/acerca">Acerca de</Link></li>
+          {/* <li><Link to="/">FormShipping</Link></li> */}
+          {/* <li><Link to="/acerca">Acerca de</Link></li>
           <li><Link to="/contacto">Contacto</Link></li>
           <li><Link to="/contador">Contador</Link></li>        
           <li><Link to="/formulario">Formulario</Link></li>  
@@ -31,12 +31,12 @@ const App: React.FC = () => {
           <li><Link to="/ImagenesSinLazy">ImagenesSinLazy</Link></li>    
           <li><Link to="/AnidarComponentes">AnidarComponentes</Link></li> 
           <li><Link to="/RenderListas">RenderListas</Link></li> 
-          <li><Link to="/FormShipping">FormShipping</Link></li> 
+          <li><Link to="/FormShipping">FormShipping</Link></li>  */}
         </ul>
       </nav>
       <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/acerca" element={<Acerca />} />
+        <Route path="/" element={<FormShipping />} />
+        {/* <Route path="/acerca" element={<Acerca />} />
         <Route path="/contacto" element={<Contacto />} />
         <Route path="/contador" element={<Contador />} />
         <Route path="/formulario" element={<Formulario />} />
@@ -47,7 +47,7 @@ const App: React.FC = () => {
         <Route path="/ImagenesSinLazy" element={<ImagenesSinLazy />} />
         <Route path="/AnidarComponentes" element={<AnidarComponentes />} />
         <Route path="/RenderListas" element={<RenderListas />} />
-        <Route path="/FormShipping" element={<FormShipping />} />
+        <Route path="/FormShipping" element={<FormShipping />} /> */}
       </Routes>
     </Router>
   );

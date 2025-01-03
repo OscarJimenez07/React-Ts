1ed76aab431c58bfec6186678531b6c6bd967375
oscarjimenez07 <ojavierjimenez@uts.edu.co>
2025-01-03 14:48:06 -0500

    Actualizacion de ruta

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://oscarjimenez07.github.io/React-Ts/FormShipping',
+  base: 'https://oscarjimenez07.github.io/React-Ts/',
 })

2b4f7fd4a0cda132ee011a2482b8093d7ddf2c56
oscarjimenez07 <ojavierjimenez@uts.edu.co>
2025-01-02 22:48:04 -0500

    Se añaden rutas al incio para que sea reconocido en el action del git hub page

--- src/App.tsx
+++ src/App.tsx
@@ -6,42 +6,48 @@ import Contacto from './pages/Contacto';
 import Contador from './pages/Contador'
 import Formulario from './components/FormularioRegistro'
 import ComponentePadre from './components/componentePadre';
-import ComPadreRMemo from './components/comPadreRMemo'
+import ComPadreRMemo from './components/ComPadreRMemo'
 import LayLoading from './components/LazyLoadingP'
 import LazyLoaingImagen from './components/LazyLoadingImagen'
 import ImagenesSinLazy from './components/ImagenesSinLazy'
 import AnidarComponentes from './components/AnidarComponentes'
+import RenderListas from './components/RenderListas'
+import FormShipping from './components/FormShipping'
 
 const App: React.FC = () => {
   return (
     <Router>
       <nav>
         <ul>
-          <li><Link to="/">Inicio</Link></li>
-          <li><Link to="/acerca">Acerca de</Link></li>
-          <li><Link to="/contacto">Contacto</Link></li>
-          <li><Link to="/contador">Contador</Link></li>        
-          <li><Link to="/formulario">Formulario</Link></li>  
-          <li><Link to="/ComponentePadre">Props</Link></li>     
-          <li><Link to="/comPadreRMemo">Optimizador</Link></li>     
-          <li><Link to="/LayLoading">LayLoading</Link></li>    
-          <li><Link to="/LazyLoaingImagen">LazyLoaingImagen</Link></li>    
-          <li><Link to="/ImagenesSinLazy">ImagenesSinLazy</Link></li>    
-          <li><Link to="/AnidarComponentes">AnidarComponentes</Link></li> 
+          <li><Link to="/React-Ts/">Inicio</Link></li>
+          <li><Link to="/React-Ts/acerca">Acerca de</Link></li>
+          <li><Link to="/React-Ts/contacto">Contacto</Link></li>
+          <li><Link to="/React-Ts/contador">Contador</Link></li>        
+          <li><Link to="/React-Ts/formulario">Formulario</Link></li>  
+          <li><Link to="/React-Ts/ComponentePadre">Props</Link></li>     
+          <li><Link to="/React-Ts/comPadreRMemo">Optimizador</Link></li>     
+          <li><Link to="/React-Ts/LayLoading">LayLoading</Link></li>    
+          <li><Link to="/React-Ts/LazyLoaingImagen">LazyLoaingImagen</Link></li>    
+          <li><Link to="/React-Ts/ImagenesSinLazy">ImagenesSinLazy</Link></li>    
+          <li><Link to="/React-Ts/AnidarComponentes">AnidarComponentes</Link></li> 
+          <li><Link to="/React-Ts/RenderListas">RenderListas</Link></li> 
+          <li><Link to="/React-Ts/FormShipping">FormShipping</Link></li> 
         </ul>
       </nav>
       <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/acerca" element={<Acerca />} />
-        <Route path="/contacto" element={<Contacto />} />
-        <Route path="/contador" element={<Contador />} />
-        <Route path="/formulario" element={<Formulario />} />
-        <Route path="/ComponentePadre" element={<ComponentePadre />} />
-        <Route path="/ComPadreRMemo" element={<ComPadreRMemo />} />
-        <Route path="/LayLoading" element={<LayLoading />} />
-        <Route path="/LazyLoaingImagen" element={<LazyLoaingImagen />} />
-        <Route path="/ImagenesSinLazy" element={<ImagenesSinLazy />} />
-        <Route path="/AnidarComponentes" element={<AnidarComponentes />} />
+        <Route path="/React-Ts/" element={<Inicio />} />
+        <Route path="/React-Ts/acerca" element={<Acerca />} />
+        <Route path="/React-Ts/contacto" element={<Contacto />} />
+        <Route path="/React-Ts/contador" element={<Contador />} />
+        <Route path="/React-Ts/formulario" element={<Formulario />} />
+        <Route path="/React-Ts/ComponentePadre" element={<ComponentePadre />} />
+        <Route path="/React-Ts/ComPadreRMemo" element={<ComPadreRMemo />} />
+        <Route path="/React-Ts/LayLoading" element={<LayLoading />} />
+        <Route path="/React-Ts/LazyLoaingImagen" element={<LazyLoaingImagen />} />
+        <Route path="/React-Ts/ImagenesSinLazy" element={<ImagenesSinLazy />} />
+        <Route path="/React-Ts/AnidarComponentes" element={<AnidarComponentes />} />
+        <Route path="/React-Ts/RenderListas" element={<RenderListas />} />
+        <Route path="/React-Ts/FormShipping" element={<FormShipping />} />
       </Routes>
     </Router>
   );
